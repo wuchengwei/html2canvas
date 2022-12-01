14fcb01e2c6ebcac8793221d082529a466282b6d
Chengwei Wu <meegodevelop@gmail.com>
2022-12-01 22:19:01 +0800

    fix svg screenshot offset issue when it has margin

--- src/dom/replaced-elements/svg-element-container.ts
+++ src/dom/replaced-elements/svg-element-container.ts
@@ -13,6 +13,18 @@ export class SVGElementContainer extends ElementContainer {
         const bounds = parseBounds(context, img);
         img.setAttribute('width', `${bounds.width}px`);
         img.setAttribute('height', `${bounds.height}px`);
+        // 以下设置margin的代码修复在svg元素被设置了margin的时候导致的截屏偏移问题
+        img.style.margin = '0';
+        img.style.marginTop = '0';
+        img.style.marginBottom = '0';
+        img.style.marginLeft = '0';
+        img.style.marginRight = '0';
+        img.style.marginInline = '0';
+        img.style.marginInlineStart = '0';
+        img.style.marginInlineEnd = '0';
+        img.style.marginBlock = '0';
+        img.style.marginBlockStart = '0';
+        img.style.marginBlockEnd = '0';
 
         this.svg = `data:image/svg+xml,${encodeURIComponent(s.serializeToString(img))}`;
         this.intrinsicWidth = img.width.baseVal.value;
